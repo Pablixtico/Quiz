8883816bf2aa157e4cfa155b808f12b7fd56219c
Pablixtico <pablo.gimenez.ricart@gmail.com>
2024-03-06 13:12:43 +0100

    added final screen for questions, added music and background corrections, correct the timing between questions, more styling, correct decode of html

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 "use strict"
 let datos;
-
+let clicked = false;
 
 window.onload = recibir();
 
@@ -65,8 +65,15 @@ function loadActualQuestion(){
 
 
 function handleclick(e){
+
+  if(clicked){
+    return;
+  }
+
+  clicked = true;
+
   document.getElementsByClassName("quiz-card")[0].classList.remove("animate__wobble")
-  answers = [...answers,{"correct":datos[actualquestion].correct_answer,"checked":e.target.textContent}]
+  answers = [...answers,{"correct":decodeHtml(datos[actualquestion].correct_answer),"checked":decodeHtml(e.target.textContent)}]
   console.log(answers);
 
 if(e.target.textContent!=decodeHtml(datos[actualquestion].correct_answer)){
@@ -80,9 +87,14 @@ if(e.target.textContent!=decodeHtml(datos[actualquestion].correct_answer)){
     e.target.style.color = "#f2f2f2"
     e.target.textContent = "✔️ " + e.target.textContent
   }
+  if(actualquestion>=9){
+    setTimeout(() => {
+      finalQuiz();
+    }, 1000);
+  }
   actualquestion++;
-  console.log(actualquestion)
   setTimeout(() => {
+    clicked = false;
     document.getElementsByClassName("quiz-card")[0].classList.add("animate__wobble")
     loadActualQuestion();
   }, 1500);
@@ -99,6 +111,52 @@ function checkCorrect(){
   }
 }
 
+
+function finalQuiz(){
+
+  document.getElementsByClassName("quiz-container")[0].style.display = "none";
+  let corrects = 0;
+ 
+    let x = 0;
+
+  for(let an of answers){
+    let divcontainer = document.createElement("div");
+    let h2 = document.createElement("h2");
+    let divbuttons = document.createElement("div");
+    let opt1 = document.createElement("div");
+    let opt2 = document.createElement("div");
+    divcontainer.classList.add("final");
+    divbuttons.classList.add("buttons");
+    opt1.classList.add("final1");
+    opt2.classList.add("final2");
+    opt1.style.backgroundColor = "#32cd32"
+    opt2.style.backgroundColor = "#ED4337"
+    opt1.style.color = "white"
+    opt2.style.color = "white"
+    divbuttons.appendChild(opt1);
+    if(an.correct != an.checked){
+      divbuttons.appendChild(opt2);
+    }
+    divcontainer.appendChild(h2);
+    divcontainer.appendChild(divbuttons);
+    h2.textContent = decodeHtml(datos[x].question);
+    opt1.textContent = an.correct;
+    opt2.textContent = an.checked;
+    opt1.textContent += " ✔️";
+    opt2.textContent += " ❌";
+    document.getElementsByClassName("final-questions")[0].appendChild(divcontainer);
+    x++
+  }
+
+
+  for(let ans of answers){
+      if(ans.correct === ans.checked){
+        corrects++;
+        console.log(ans);
+      }
+  }
+
+}
 
 function decodeHtml(html) {
   var txt = document.createElement("textarea");
